349b05e1b44a9f4aa428dd9b10656122b120ebaa
Mohammed-Kanbari <mohammed.2aq2@gmail.com>
2025-03-31 19:59:41 +0400

    first commit

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,9 @@
+import React , {ReactNode} from 'react'
+
+const RootLayout = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div> Children</div>
+  )
+}
+
+export default RootLayout
