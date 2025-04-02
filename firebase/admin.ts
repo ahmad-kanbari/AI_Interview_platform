c72da837acbee527a52fb4df8c11fcec2ab31529
ahmad-kanbari <ahmadkanbari.personal@gmail.com>
2025-04-02 06:17:25 +0400

    fixing client base

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -7,13 +7,11 @@ import { getFirestore } from 'firebase-admin/firestore';
 const initFirebaseAdmin = () => { 
     const apps = getApps();
 
-    console.log('ahmad' ,process.env.FIREBASE_PROJECT_ID);
-    console.log('ahmad' ,process.env.FIRBASE_CLIENT_EMAIL);
     if (!apps.length) {
         initializeApp({
             credential: cert({
                 projectId: process.env.FIREBASE_PROJECT_ID,
-                clientEmail:process.env.FIRBASE_CLIENT_EMAIL,
+                clientEmail:process.env.FIREBASE_CLIENT_EMAIL,
                 privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
             }),
 
